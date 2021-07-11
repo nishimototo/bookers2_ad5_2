09f927b6c010e65b31e9ec0ae6ce938742aa7cf4
nishimototo <dkspm2436@gmail.com>
2021-07-11 13:54:52 +0000

    follow follower一覧まで実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,20 @@ class User < ApplicationRecord
   validates :name, uniqueness: true, length: {in: 2..20}
   validates :introduction, length: {maximum: 50}
 
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
   has_many :books, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :comments, dependent: :destroy
 
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  has_many :active_relationships, class_name: "Relationship", foreign_key: "following_id", dependent: :destroy
+  has_many :followings, through: :active_relationships, source: :follower
+
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: "follower_id"
+  has_many :followers, through: :passive_relationships, source: :following
+
+  def followed_by?(user)
+    passive_relationships.where(following_id: user.id).exists?
+  end
 end
